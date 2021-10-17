23dbee06d061962b90629108fee53eca7ce9e33a
Vadim Ledyaev <vl@pmobi.tech>
2021-10-17 18:39:12 +0700

    WalletType to provider WalletContractType conversion extension

--- lib/src/provider/models/wallet_contract_type.dart
+++ lib/src/provider/models/wallet_contract_type.dart
@@ -1,6 +1,9 @@
 import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+import '../../core/accounts_storage/models/multisig_type.dart';
+import '../../core/accounts_storage/models/wallet_type.dart';
+
 part 'wallet_contract_type.g.dart';
 
 @HiveType(typeId: 221)
@@ -23,4 +26,24 @@ enum WalletContractType {
   @HiveField(5)
   @JsonValue('WalletV3')
   walletV3,
+}
+
+extension WalletTypeX on WalletType {
+  WalletContractType toWalletType() => when(
+        multisig: (multisigType) {
+          switch (multisigType) {
+            case MultisigType.safeMultisigWallet:
+              return WalletContractType.safeMultisigWallet;
+            case MultisigType.safeMultisigWallet24h:
+              return WalletContractType.safeMultisigWallet24h;
+            case MultisigType.setcodeMultisigWallet:
+              return WalletContractType.setcodeMultisigWallet;
+            case MultisigType.bridgeMultisigWallet:
+              return WalletContractType.bridgeMultisigWallet;
+            case MultisigType.surfWallet:
+              return WalletContractType.surfWallet;
+          }
+        },
+        walletV3: () => WalletContractType.walletV3,
+      );
 }
