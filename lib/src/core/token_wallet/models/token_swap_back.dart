ebb7103e514c454500f05d797bdbacea4ea75c3d
Vadim Ledyaev <vl@pmobi.tech>
2021-10-18 16:55:17 +0700

    Android minSdkVersion changed, transactions parsing fix

--- lib/src/core/token_wallet/models/token_swap_back.dart
+++ lib/src/core/token_wallet/models/token_swap_back.dart
@@ -6,10 +6,11 @@ part 'token_swap_back.g.dart';
 
 @freezed
 class TokenSwapBack with _$TokenSwapBack {
-  @JsonSerializable()
+  @JsonSerializable(fieldRename: FieldRename.snake)
   const factory TokenSwapBack({
     required String tokens,
-    required String to,
+    required String callbackAddress,
+    required String callbackPayload,
   }) = _TokenSwapBack;
 
   factory TokenSwapBack.fromJson(Map<String, dynamic> json) => _$TokenSwapBackFromJson(json);
