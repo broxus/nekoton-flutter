52717117cad1ba4e12d1ea700413fb9c0c3ec36f
Vadim Ledyaev <vl@pmobi.tech>
2022-01-17 21:24:28 +0700

    Synchronized update

--- lib/src/core/accounts_storage/models/native_accounts_storage.dart
+++ lib/src/core/accounts_storage/models/native_accounts_storage.dart
@@ -1,41 +1,38 @@
 import 'dart:async';
 import 'dart:ffi';
 
-import 'package:synchronized/synchronized.dart';
-
 import '../../../ffi_utils.dart';
 import '../../../models/nekoton_exception.dart';
 import '../../../nekoton.dart';
 
 class NativeAccountsStorage {
-  final _lock = Lock();
   Pointer<Void>? _ptr;
 
   NativeAccountsStorage(this._ptr);
 
   bool get isNull => _ptr == null;
 
-  Future<int> use(Future<int> Function(Pointer<Void> ptr) function) => _lock.synchronized(() async {
-        if (_ptr == null) {
-          throw AccountStorageNotFoundException();
-        } else {
-          return function(_ptr!);
-        }
-      });
+  Future<int> use(Future<int> Function(Pointer<Void> ptr) function) async {
+    if (_ptr == null) {
+      throw AccountStorageNotFoundException();
+    } else {
+      return function(_ptr!);
+    }
+  }
 
-  Future<void> free() => _lock.synchronized(() async {
-        if (_ptr == null) {
-          throw AccountStorageNotFoundException();
-        } else {
-          final ptr = _ptr;
-          _ptr = null;
+  Future<void> free() async {
+    if (_ptr == null) {
+      throw AccountStorageNotFoundException();
+    } else {
+      final ptr = _ptr;
+      _ptr = null;
 
-          await proceedAsync(
-            (port) => nativeLibraryInstance.bindings.free_accounts_storage(
-              port,
-              ptr!,
-            ),
-          );
-        }
-      });
+      await proceedAsync(
+        (port) => nativeLibraryInstance.bindings.free_accounts_storage(
+          port,
+          ptr!,
+        ),
+      );
+    }
+  }
 }
--- lib/src/external/models/native_gql_connection.dart
+++ lib/src/external/models/native_gql_connection.dart
@@ -1,41 +1,38 @@
 import 'dart:async';
 import 'dart:ffi';
 
-import 'package:synchronized/synchronized.dart';
-
 import '../../ffi_utils.dart';
 import '../../models/nekoton_exception.dart';
 import '../../nekoton.dart';
 
 class NativeGqlConnection {
-  final _lock = Lock();
   Pointer<Void>? _ptr;
 
   NativeGqlConnection(this._ptr);
 
   bool get isNull => _ptr == null;
 
-  Future<int> use(Future<int> Function(Pointer<Void> ptr) function) => _lock.synchronized(() async {
-        if (_ptr == null) {
-          throw GqlConnectionNotFoundException();
-        } else {
-          return function(_ptr!);
-        }
-      });
+  Future<int> use(Future<int> Function(Pointer<Void> ptr) function) async {
+    if (_ptr == null) {
+      throw GqlConnectionNotFoundException();
+    } else {
+      return function(_ptr!);
+    }
+  }
 
-  Future<void> free() => _lock.synchronized(() async {
-        if (_ptr == null) {
-          throw GqlConnectionNotFoundException();
-        } else {
-          final ptr = _ptr;
-          _ptr = null;
+  Future<void> free() async {
+    if (_ptr == null) {
+      throw GqlConnectionNotFoundException();
+    } else {
+      final ptr = _ptr;
+      _ptr = null;
 
-          await proceedAsync(
-            (port) => nativeLibraryInstance.bindings.free_gql_connection(
-              port,
-              ptr!,
-            ),
-          );
-        }
-      });
+      await proceedAsync(
+        (port) => nativeLibraryInstance.bindings.free_gql_connection(
+          port,
+          ptr!,
+        ),
+      );
+    }
+  }
 }
