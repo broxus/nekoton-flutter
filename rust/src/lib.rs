a65a607e3e958363ce0b7a5bc3f57d7a6dff98c2
Vladimir <hello@0xdeafbeef.dev>
2022-12-02 14:08:05 +0100

    change ptr parsing from usize to u64

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -72,6 +72,16 @@ pub unsafe extern "C" fn nt_cstring_to_void_ptr(ptr: *mut c_char) -> *mut c_void
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn nt_void_ptr_to_c_str(ptr: *mut c_void) -> *mut c_char {
+
+    let string = (ptr as u64).to_string();
+    let c_string = CString::new(string).unwrap();
+
+    c_string.into_raw()
+}
+
+
+#[no_mangle]
 pub unsafe extern "C" fn nt_free_cstring(ptr: *mut c_char) {
     ptr.to_string_from_ptr();
 }
@@ -151,7 +161,7 @@ pub trait ToPtrAddress {
 
 impl<T> ToPtrAddress for *mut T {
     fn to_ptr_address(self) -> String {
-        (self as usize).to_string()
+        (self as u64).to_string()
     }
 }
 
@@ -161,7 +171,7 @@ pub trait ToPtrFromAddress {
 
 impl ToPtrFromAddress for String {
     fn to_ptr_from_address<T>(self) -> *mut T {
-        self.parse::<usize>().unwrap() as *mut T
+        self.parse::<u64>().unwrap() as *mut T
     }
 }
 
